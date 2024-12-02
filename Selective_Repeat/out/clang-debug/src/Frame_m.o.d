e439492adaaab5123e34cf518b005711a3c08fe8
mennamohamed0207 <menamohamed0207@gmail.com>
2024-12-02 18:26:24 +0200

    Adding variables and intializing structure

--- Selective_Repeat/out/clang-debug/src/Frame_m.o.d
+++ Selective_Repeat/out/clang-debug/src/Frame_m.o.d
@@ -1,6 +1,6 @@
-../out/clang-debug/src/Coordinator.o: Coordinator.cc Coordinator.h \
+../out/clang-debug/src/Frame_m.o: Frame_m.cc Frame_m.h \
   E:/SSD/omnetpp-6.0.3-windows-x86_64/omnetpp-6.0.3/include/omnetpp.h
 
-Coordinator.h:
+Frame_m.h:
 
 E:/SSD/omnetpp-6.0.3-windows-x86_64/omnetpp-6.0.3/include/omnetpp.h:
--- Selective_Repeat/src/Node.cc
+++ Selective_Repeat/src/Node.cc
@@ -16,13 +16,62 @@
 #include "Node.h"
 
 Define_Module(Node);
+//Sender variable
+int WS;
+int sender_start_index=0;
+int sender_end_index=WS-1;
+int sender_index=0;
+std::vector<Frame_Base*> sender_buffer(WS);
+int ack_expected=0;
+int next_frame_to_send=0;
+bool no_nack=true;
+
+//Receiver Variable
+int receiver_start_index=0;
+int receiver_end_index=WS-1;
+int receiver_index=0;
+std::vector<Frame_Base*> receiver_buffer(WS);
+std::vector<bool> arrived(WS,false);
+int frame_expected=0;
+Frame_Base* frame;
+bool is_sender=true;
+
+enum event_type{frame_arrival,CRC_error,frame_timeout,ack_timeout};
+event_type event;
 
 void Node::initialize()
 {
     // TODO - Generated method body
+    WS=this->getParentModule()->par("WS").intValue();
 }
 
 void Node::handleMessage(cMessage *msg)
 {
     // TODO - Generated method body
+    //check if it is sender node or receiver based on coordinator
+    if(is_sender) //should be modified
+    {
+
+
+    }else {
+
+    }
 }
+
+std::vector<string> Node::readFile(string path)
+{
+    //TODO
+}
+std::string Node::framing(string line)
+{
+    //TODO
+}
+std::string Node::CRC(string frame)
+{
+    //TODO
+}
+void Node::sendMessage(string message)
+{
+    //TODO
+}
+
